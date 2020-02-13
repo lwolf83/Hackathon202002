9391dfa9c021f8717b86fc0e218853d3b0543388
Laure90 <58706312+Laure90@users.noreply.github.com>
2020-02-13 18:08:23 +0100

    ajout requete sql category id et name

--- ProductsModule.cs
+++ ProductsModule.cs
@@ -6,7 +6,7 @@ namespace EcoConception
 {
     public class ProductsModule : AbstractModule
     {
-        public override IEnumerable<Product> Products { get => Database.GetAllProducts();  }
+        public override IEnumerable<Product> Products { get; }
         public override IEnumerable<Category> Categories { get; }
 
         public ProductsModule()
